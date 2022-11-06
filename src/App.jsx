56c85b3538e472f15b63ed19426ab206cc755970
rajeevrk9901 <rajeevrk9901@gmail.com>
2022-11-06 08:55:56 +0530

    added flex

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,22 @@ function App() {
   return (
     // <h1>hi</h1>
     <>
-      <div className="wrapper flex flex-col gap-2">
-        <img className='w-10' src={star} alt="" srcset="" />
-        <h2>How did we do?</h2>
-        <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-        <ul>
-          <li><button>1</button></li>
-          <li><button>2</button></li>
-          <li><button>3</button></li>
-          <li><button>4</button></li>
-          <li><button>5</button></li>
-        </ul>
+      {/* Global Container */}
+      <div className="wrapper flex flex-col justify-center items-center min-h-screen">
+        {/* Local Container */}
+        <div className="flex flex-col items-center px-4 space-y-4">
+          <img className='w-10' src={star} alt="" srcset="" />
+          <h2>How did we do?</h2>
+          <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+          <ul className='flex space-x-4'>
+            <li><button>1</button></li>
+            <li><button>2</button></li>
+            <li><button>3</button></li>
+            <li><button>4</button></li>
+            <li><button>6</button></li>
+          </ul>
+          <button type="submit">submit</button>
+        </div>
       </div>
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import checker from 'vite-plugin-checker'
+// export default {
+
+// }
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  checka: [checker({ typescript: true })], // e.g. use TypeScript check
 })
